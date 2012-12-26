773fd37628f7962ed19ba94e1e27a0bb77256282
root <root@c02.cloudiya.com>
2012-12-26 16:30:01 +0800

    auto update data in cloudiya

--- TestCode.lua
+++ TestCode.lua
@@ -1,3 +1,22 @@
 
 local i = 1
 print(i)
+
+
+tableA = {"jack","jilly","moon","beijing"}
+
+for k,val in pairs(tableA) 
+do 
+ print(k,val) 
+end
+
+
+tableB = {name="jack",age="10",mark="100"}
+
+for k,val in pairs(tableB) do print(k,val) end
+
+
+tableC = {["name"]="terry",["age"]=10,["mark"]=90}
+
+for k ,val in pairs(tableC) do print(k,val) end
+
