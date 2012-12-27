6191dfd633912fa0d22d9596506014d89ddf1cd0
root <root@c02.cloudiya.com>
2012-12-27 14:30:01 +0800

    auto update data in cloudiya

--- TestCode.lua
+++ TestCode.lua
@@ -27,14 +27,46 @@ tableC = {["name"]="terry",["age"]=10,["mark"]=90}
 
 for k ,val in pairs(tableC) do print(k,val) end
 
-
+print(table.getn({10,2,3}))
+print(table.getn(tableA))
 
 --##################
 
+--[[
 print "enter a number : "
 
 n = assert(io.read("*number"),"invalid input")
+--]]
 
+--##################
 
+tableD = {}
+table.insert(tableD,10)
+print(table.getn(tableD))
 
+--##################
+
+lines = {
+ 
+      luaH_set = 10,
+      luaH_get = 24,
+      luaH_preset = 48,
+     
+}
+
+a = {}
+
+for n in pairs(lines) 
+ do
+   table.insert(a,n)
+ end   
+
+table.sort(a)
+
+for i,n in ipairs(a)
+ do
+   print(i,n)
+ end
+
+--##################
 
