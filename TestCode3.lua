06ea549ea1684b55763d3c99a40450480a4f3641
root <root@c02.cloudiya.com>
2013-05-17 11:30:02 +0800

    auto update data in cloudiya

--- TestCode3.lua
+++ TestCode3.lua
@@ -1,3 +1,5 @@
+local cjson = require "cjson"
+
 
 for i=2,4,1 do
     print(i)
@@ -21,3 +23,23 @@ end
 
 
 print(htgetn(tt))
+
+print("------")
+
+SegmentFlagDict = {1,1,1,1,6,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0}
+
+table.remove(SegmentFlagDict,4)
+
+print(SegmentFlagDict[5])
+
+
+
+
+                        for i,v in ipairs(SegmentFlagDict) do
+                            if tonumber(v) == 0 then
+                               table.remove(SegmentFlagDict,i)
+                               print(cjson.encode(SegmentFlagDict))
+                            end
+                        end
+
+print(cjson.encode(SegmentFlagDict))
