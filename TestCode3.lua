81553a2765969e456d65a40986f6c639b82d85ca
root <root@c02.cloudiya.com>
2013-05-17 10:30:01 +0800

    auto update data in cloudiya

--- TestCode3.lua
+++ TestCode3.lua
@@ -2,3 +2,22 @@
 for i=2,4,1 do
     print(i)
 end
+
+tt = {2,7,9}
+
+for i,v in ipairs(tt) do
+
+    print(i,v)
+end
+
+
+                     function htgetn(hashtable)
+                        local n = 0
+                        for _,v in pairs(hashtable) do
+                            n = n + 1
+                        end
+                        return n
+                     end
+
+
+print(htgetn(tt))
