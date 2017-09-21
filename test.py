df506ea50f2a51cdfb9a2a573d36b1530c477c1b
itwye <itwye@qq.com>
2017-09-21 17:19:57 +0800

    sublime text git plugin test!

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-09-21 17:18:26
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+import os
+
+print "hello world!"
